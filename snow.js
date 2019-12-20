c9918e5d82dee3de15e6fa2f4eab7eeb0904ebf2
zxyz <cmp@airmail.cc>
2019-12-20 23:20:49 +0200

    lol

--- snow.js
+++ snow.js
@@ -2,8 +2,52 @@ var snowFall = document.getElementById("snow");
 
 var randInt = function(max) {
 	max += 1;
-	return Math.floor(Math.random() * max);
+	return Math.floor(Math.random() * max) + 1;
 };
+
+var fallDirection = "right";
+var _xpos = 0;
+var _ypos = 0;
+
+function findObjectCoords(mouseEvent)
+{
+// spaghetti thing from some blog
+
+  var obj = document.body;
+  var obj_left = 0;
+  var obj_top = 0;
+  var xpos;
+  var ypos;
+  while (obj.offsetParent)
+  {
+    obj_left += obj.offsetLeft;
+    obj_top += obj.offsetTop;
+    obj = obj.offsetParent;
+  }
+  if (mouseEvent)
+  {
+    //Firefox
+    xpos = mouseEvent.pageX;
+    ypos = mouseEvent.pageY;
+  }
+  else
+  {
+    //IE
+    xpos = window.event.x + document.body.scrollLeft - 2;
+    ypos = window.event.y + document.body.scrollTop - 2;
+  }
+  xpos -= obj_left;
+  ypos -= obj_top;
+
+//  document.getElementById("objectCoords").innerHTML = xpos + ", " + ypos;
+
+  if (xpos > _xpos + 10) fallDirection = "right";
+  else if (xpos < _xpos - 10) fallDirection = "left";
+
+  _xpos = xpos;
+  _ypos = ypos;
+
+}
 
 var generateFlakes = setInterval(function() {
 	var height = 0;
@@ -21,15 +65,21 @@ var moveFlakes = setInterval(function() {
 	for (var i = allFlakes.length - 1; i > -1; --i) {
 		var height = parseFloat(allFlakes[i].getAttribute("data-height"));
 		var width = parseFloat(allFlakes[i].getAttribute("data-width"));
-		if (height == 100 ||
-			width == 100) {
+		if (height >= 100 ||
+			width >= 100 ||
+			width <= 0) {
 			allFlakes[i].parentNode.removeChild(allFlakes[i]);
 		} else {
 			height += 0.1;
-			width += 0.1;
+			if (fallDirection == "right")
+				width += 0.1;
+			else if (fallDirection == "left")
+				width -= 0.1;
 			allFlakes[i].setAttribute("data-height", height);
 			allFlakes[i].setAttribute("data-width", width);
 			allFlakes[i].setAttribute("style", "top:"+height+"%;left:"+width+"%;");
 		}
 	}
 }, 10);
+
+document.body.onmousemove = findObjectCoords;
