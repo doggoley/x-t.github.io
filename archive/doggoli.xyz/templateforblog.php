86a7caa3c83b336b9da344f43091fb0939bc33c5
zxyz <cmp@airmail.cc>
2020-01-02 22:53:58 +0200

    unofficial remaster

---
+++ archive/doggoli.xyz/templateforblog.php
@@ -0,0 +1,129 @@
+<html>
+	<head>
+
+		<title>Doggoli</title>
+   	 	<meta name="description" content="Hi, i'm Doggoli.">
+		<style>
+
+			
+		.blogtitle {
+
+    	text-align: center;
+    	font-size: 20px;
+  		font-style: italic;
+    	font-family: courier new;
+    	color: #ea650b;
+    	
+}
+
+		body {
+
+   		font-family: monospace;
+    	background-color: #414a4c;
+
+}
+	
+		.textdiv {
+
+			text-align: center;
+			color: #ea650b;
+
+
+
+}
+
+		.img {
+
+			text-align: center;
+
+		}
+
+		.titlediv {
+
+		text-align: center;
+		color: #ea650b;
+		font-size: 48px;
+
+
+
+
+
+	
+		.nigger {
+
+		font-size: 14px;
+		left:0;
+	    bottom: 0;
+	    width: 100%;
+	    text-align: center;
+	    color: #ea650b;
+	    font-family: Helvetica Neue;
+
+}
+
+		.rip {
+
+
+			text-align: center;
+			font-size: 30px;
+			font-family: Helvetica Neue;
+
+
+		}
+
+
+
+
+
+
+		</style>	
+	</head>
+
+<?php
+// This is for analytics or something.
+$utime = strval(time());
+file_put_contents("/home/www-data/doggoli-visitors.log", "[{$utime}]IP {$_SERVER['REMOTE_ADDR']} viewed the blog page.\n", FILE_APPEND);
+
+// beta testing - new analytics engine
+include('/home/www-data/analytics.php');
+
+?>
+
+
+
+	<body>
+
+		<div class="titlediv">
+		<p>Blog</p>
+		</div>
+			
+		<div class="blogtitle">
+		<p>Pretty much just a radio that broadcasts my entire music library, shuffled. It won't broadcast every day, nor is there a schedule for when the broadcast starts (im too busy for that shit). Enjoy the shity music and if you are an artist that is against me playing your music on this station, contact me at: idontgiveashit@fuckoff.com</p>
+		</div>
+
+		
+		
+
+	
+
+	
+
+
+
+
+		<div class="nigger">
+
+   		 <p>(C) 2018 Doggoli. If you copy it i will be mad, but, buy me a coke and we're cool.</p>
+			
+   		
+   		</div>        
+
+
+    <br>
+
+
+
+	</body>
+
+
+</html>
---
+++ archive/doggoli.xyz/themeaningoflife.php
@@ -0,0 +1,168 @@
+<html>
+<head>
+    <title>Doggoli</title>
+    <meta name="description" content="Hi, i'm Doggoli.">
+<style>
+
+#cdiv {
+    text-align: center;
+    font-size: 30.5px;
+    font-family: Helvetica Neue;
+
+    
+}
+
+#cuntdiv {
+    text-align: center;
+    font-size: 40px;
+    font-family: Helvetica Neue;
+    font-style: italic;
+}
+
+.text {
+
+    text-align: center;
+    font-size: 16px;
+    font-family: Helvetica Neue;
+   
+    
+
+}
+
+.nigger {
+
+    font-size: 12px;
+    position: static;
+    left:0;
+    bottom: 0;
+    width: 100%;
+    text-align: center;
+    font-family: Helvetica Neue;
+}
+
+.title {
+
+    font-size: 40px;
+    font-weight: lighter;
+    font-family: Helvetica Neue;
+    font-style: italic;
+
+}
+
+.list {
+
+    font-family: Helvetica Neue;
+    font-size: 16;
+    
+}
+
+@font-face {
+    font-family: Helvetica Neue;
+    src: url(fuckingfont.ttf);
+}
+
+body {
+    color: #ff6e00;
+    background-color: #414a4c;
+    text-align: center;
+
+}
+
+a {
+    color: #ea650b;
+}
+</style>
+</head>
+<body>
+<?php
+// This is for analytics or something.
+$utime = strval(time());
+file_put_contents("/home/www-data/doggoli-visitors.log", "[{$utime}]IP {$_SERVER['REMOTE_ADDR']} viewed the contact page.\n", FILE_APPEND);
+
+// beta testing - new analytics engine
+include('/home/www-data/analytics.php');
+
+?>
+
+    <div id="cdiv">
+    
+    <p>Its actually not....</p>
+            
+    </div>        
+  
+
+    <div id="cuntdiv">    
+        
+	
+        <p> Nor is your dick any bigger than mine!</p>
+
+
+    </div>
+    
+    <div class="text">   
+
+        <p>If you didn't manage to get the metaphor, i am taking about computers. You can find these people anywhere, radiating their shitty hate cloud of insults (macfag, winfag...etc). Here's how to reason with them... you cannot. Any reasonable argument you deal to them they will reject in order to fit their shitty agenda.</p>
+    
+
+    </div>
+
+
+    <div class="title">
+
+        <p>How not to be a fanboy 101.</p>
+
+
+    </div>
+
+
+    <div class="list">
+        <li> Understand that nothing is fucking perfect.</li>
+        <li> Understand that just because you like it doesn't mean other people also do!</li>
+        <li> Acknowledge that every product, including the one you have, has faults.</li>
+        <li> Do not deny your experience!</li>
+
+        <p>Confused? Watch this video.</p>
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/_hYHwkIuEMo?ecver=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>
+        <p>Your browser doesn't support iframes.</p>
+        <a href="https://www.youtube.com/watch?v=_hYHwkIuEMo">Video link</a>
+        </iframe>
+
+    </div>
+
+    <div class="title">
+
+     <p> MFW Macfag cannot understand the joke lmao!!!!! /r/woosh :DDDD ebin </p>
+     
+    </div>   
+
+
+    <div class="text">
+
+        <p>If only it was all a fucking joke... Dont believe me?</p>
+        
+        <a href="https://boards.4chan.org/g/">A magical place named /g/ is here to tell you otherwise</a>
+
+    </div>
+
+    <div class="title">
+        <p> Moral of the story.</p>
+
+    </div>
+
+
+    <div class="text">
+
+    <p> Use what you like and like what you use, but don't make other people hate and feel ashamed for liking something different!</p>
+
+    </div>
+
+      <br>
+
+    <div class="nigger">
+    <p>(C) 2018 Doggoli. If you copy it i will be mad, but, buy me a coke and we're cool.</p>
+    </div>           
+  
+    
+</body>
+</html>
